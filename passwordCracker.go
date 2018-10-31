90a67b65e4e08fc59d4079397c16297255ec8af8
Your Name <you@example.com>
2018-10-31 00:35:40 -0400

    added in user message strings

--- passwordCracker.go
+++ passwordCracker.go
@@ -1,1 +1,16 @@
-rewrite this 
+// Package gx0re
+package gx0re
+
+import (
+	"fmt"
+)
+
+func PasswordCracker(str) str {
+	fmt.Println("[+] Starting the password Cracker...")
+	fmt.Println("[+] Password Found; Stopping the Password Cracker...")
+
+	fmt.Println("[-] Password Not Found...")
+	fmt.Println("[-] An Error has occured...")
+
+	fmt.Println("[+] Stopping the password Cracker...")
+}
